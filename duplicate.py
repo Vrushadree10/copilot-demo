f8b25f131f1ccecaa180d43418362f05bd30d273
deekshi13 <deekshithakb1509@gmail.com>
2025-09-02 17:18:16 +0530

    Initial commit

--- duplicate.py
+++ duplicate.py
@@ -3,7 +3,6 @@ import pandas as pd
 # Use raw string (r"") or double backslashes for Windows file paths
 df = pd.read_excel(r"C:\Users\Akshitha\Downloads\Example.xlsx")
 
-# Replace 'Logical ID' with the actual column name in your sheet
 duplicates = df[df.duplicated('Logical ID', keep=False)]
 
 print("Duplicate Logical IDs:")
